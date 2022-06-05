602ed093858e9476e6a4332724e8c5bdf83cd7b6
Pappyjay23 <pjinadu02@gmail.com>
2022-06-05 20:48:22 +0100

    Added functionality for fixed navbar on scroll to certain height

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 let menuBar = document.querySelector(".menu__bar")
 let navLinks = document.querySelector(".nav__links")
+let navbar = document.querySelector(".navbar")
 
 menuBar.addEventListener("click", ()=>{
     menuBar.classList.toggle("change")
     navLinks.classList.toggle("active")
+})
+window.addEventListener("scroll", ()=>{
+    if(pageHeight > 150){
+        navbar.classList.add("fixed")
+    }else if(pageHeight < 150){
+        navbar.classList.remove("fixed")
+    }
 })
