745af07fdde54b4b6a6b367736aadf1c960ae9e0
Pappyjay23 <pjinadu02@gmail.com>
2022-06-05 21:04:43 +0100

    Added functionality to close navbar on click of navlinks

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 let menuBar = document.querySelector(".menu__bar")
 let navLinks = document.querySelector(".nav__links")
 let navbar = document.querySelector(".navbar")
+let navMenuLinks = document.querySelectorAll(".nav__link")
 
 menuBar.addEventListener("click", ()=>{
     menuBar.classList.toggle("change")
     navLinks.classList.toggle("active")
 })
+
+navMenuLinks.forEach((link) =>{
+    link.addEventListener("click", ()=>{
+        navLinks.classList.remove("active")
+        menuBar.classList.toggle("change")
+    })
+})
+
 window.addEventListener("scroll", ()=>{
     const pageHeight = window.scrollY
     if(pageHeight > 150){
