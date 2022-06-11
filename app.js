3a30f920de87d7479ff6833d02bd628e84b829f5
Pappyjay23 <pjinadu02@gmail.com>
2022-06-11 09:58:50 +0100

    Added useful comments in javascript file

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ let navLinks = document.querySelector(".nav__links")
 let navbar = document.querySelector(".navbar")
 let navMenuLinks = document.querySelectorAll(".nav__link")
 
+// Mobile Menu Bar
 menuBar.addEventListener("click", ()=>{
     menuBar.classList.toggle("change")
     navLinks.classList.toggle("active")
 })
 
+// Close Menu Bar on Nav Link Click
 navMenuLinks.forEach((link) =>{
     link.addEventListener("click", ()=>{
         navLinks.classList.remove("active")
@@ -15,6 +17,7 @@ navMenuLinks.forEach((link) =>{
     })
 })
 
+// Smooth scroll to pages
 window.addEventListener("scroll", ()=>{
     const pageHeight = window.scrollY
     if(pageHeight > 150){
